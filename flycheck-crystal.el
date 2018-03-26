f1875e7a0fb077b01bb5bc3c19e1a67623664034
Linus Sellberg <sellberg@gmail.com>
2018-03-26 13:30:09 +0200

    Flycheck: Add project name.

--- flycheck-crystal.el
+++ flycheck-crystal.el
@@ -57,7 +57,7 @@ of the current file."
    (and
     buffer-file-name
     (locate-dominating-file buffer-file-name "shard.yml"))
-default-directory))
+   default-directory))
 
 (flycheck-define-checker crystal-build
   "A Crystal syntax checker using crystal build"
@@ -66,7 +66,9 @@ default-directory))
             "--no-codegen"
             "--no-color"
             "-f" "json"
-            source-inplace)
+            source-inplace
+            (eval (crystal-tool--project-name))
+            )
   :working-directory flycheck-crystal--find-default-directory
   :error-parser flycheck-crystal--error-parser
   :modes crystal-mode
